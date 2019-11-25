350a985679a623fcf444348dc932dff7fb7dda8c
Durian <1323807496@qq.com>
2019-11-25 18:17:25 +0800

    添加集群断路器监控

--- product-view-service-feign/src/main/java/xyz/durian/util/AccessViewService.java
+++ product-view-service-feign/src/main/java/xyz/durian/util/AccessViewService.java
@@ -6,7 +6,7 @@ import cn.hutool.http.HttpUtil;
 /**
  * Author: Durian
  * Date: 2019/11/25 17:34
- * Description: 不停访问服务的类
+ * Description: 不停访问服务的类,用于测试断路器监控
  */
 public class AccessViewService
 {
@@ -15,13 +15,20 @@ public class AccessViewService
         while (true)
         {
             ThreadUtil.sleep(1000);
-            try {
-                String html= HttpUtil.get("http://127.0.0.1:8012/productList");
-                System.out.println("html length:" + html.length());
-            }
-            catch(Exception e) {
-                System.err.println(e.getMessage());
-            }
+            access(8012);
+            access(8013);
+        }
+    }
+
+    private static void access(int port)
+    {
+        try
+        {
+            String html= HttpUtil.get(String.format("http://127.0.0.1:%d/productList",port));
+            System.out.printf("%d 地址的视图服务访问成功，返回大小是 %d%n" ,port, html.length());
+        } catch (Exception e)
+        {
+            System.err.printf("%d 地址的视图服务无法访问%n",port);
         }
     }
 }
